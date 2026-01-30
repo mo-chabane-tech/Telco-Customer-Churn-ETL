46581942638226469f99422124df4af568de82aa
mo-chabane-tech <toudertmohandchabane@gmail.com>
2026-01-30 22:08:44 +0100

    feat: added sqlite3_dtype_map function to src/utils.py

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,12 @@ import yaml
 from pathlib import Path
 from typing import Dict, Any
 
+dtype_map = {
+            "object": "text",
+            "int64": "integer",
+            "float64": "real"
+        }
+
 def get_config(path: str="config/config.yaml") -> Dict[str, Any]:
     try:
         with open(path, "r") as f:
@@ -12,3 +18,10 @@ def get_config(path: str="config/config.yaml") -> Dict[str, Any]:
     
 def ensure_dir(path: Path) -> None:
     path.mkdir(parents=True, exist_ok=True)
+
+def sqlite3_dtype_map(dtype: str) -> str:
+    try:
+        return dtype_map[dtype]
+    
+    except Exception as e:
+        raise Exception(f"Error mapping dtype: {e}")
