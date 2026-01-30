6ce84e747c74fc18b912fc87cbccf911e3d9eaa3
mo-chabane-tech <toudertmohandchabane@gmail.com>
2026-01-30 22:26:07 +0100

    feat: added src/analytics.py

---
+++ src/analytics.py
@@ -0,0 +1,44 @@
+import logging
+import sqlite3
+import pandas as pd
+from src.database import DatabaseManager
+
+logger = logging.getLogger(__name__)
+
+def get_churn_stats(db_manager: DatabaseManager, table_name: str) -> pd.DataFrame:
+    query = f"""
+    select 
+        Contract,
+        sum(case when Churn = 'Yes' then 1 else 0 end) as Churned_Count,
+        count(*) as Total_Count,
+        round(sum(case when Churn = 'Yes' then 1 else 0 end) * 100 / count(*), 2) as Churned_Percentage
+    from {table_name}
+    group by Contract;
+    """
+
+    try:
+        df = pd.read_sql_query(query, db_manager.conn)
+        logger.info("Churn stastistics retrieved successfully.")
+        return df
+    
+    except sqlite3.Error as e:
+        logger.error(f"Query execution failed: {e}")
+        raise
+
+def get_monthly_charges_stats(db_manager: DatabaseManager, table_name: str) -> pd.DataFrame:
+    query = f"""
+    select 
+        tenure,
+        MonthlyCharges,
+        Churn
+    from {table_name};
+    """
+
+    try:
+        df = pd.read_sql_query(query, db_manager.conn)
+        logger.info("Monthly charges data retrieved successfully.")
+        return df
+    
+    except sqlite3.Error as e:
+        logger.error(f"Query execution failed: {e}")
+        raise
