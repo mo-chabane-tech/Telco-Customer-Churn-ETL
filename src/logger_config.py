f67f0bba4ef98069b303ecea584b481c278333b9
mo-chabane-tech <toudertmohandchabane@gmail.com>
2026-01-30 20:01:16 +0100

    feat: added src/logger_config.py

---
+++ src/logger_config.py
@@ -0,0 +1,16 @@
+import logging
+import logging.config
+from typing import Dict, Any
+from src.utils import ensure_dir
+from pathlib import Path
+
+def setup_logger(config: Dict[str, Any]) -> None:
+    try:
+        log_path = config["paths"]["logs_file"]
+        ensure_dir(log_path.parent)
+        logging.config.dictConfig(config["logging"])
+        logging.info("Logger configured successfully.")
+
+    except Exception as e:
+        print(f"Error configuring logger: {e}")
+        raise
