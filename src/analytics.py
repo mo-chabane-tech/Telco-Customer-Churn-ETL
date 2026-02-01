1cd8590b24febf0ab0625c1f50d58d35f524b95a
mo-chabane-tech <toudertmohandchabane@gmail.com>
2026-02-01 23:57:49 +0100

    feat: added plot_payment_method to src/analytics.py

--- src/analytics.py
+++ src/analytics.py
@@ -21,7 +21,7 @@ def get_churn_stats(db_manager: DatabaseManager, table_name: str) -> pd.DataFram
         logger.info("Churn stastistics retrieved successfully.")
         return df
     
-    except sqlite3.Error as e:
+    except Exception as e:
         logger.error(f"Query execution failed: {e}")
         raise
 
@@ -39,7 +39,7 @@ def get_monthly_charges_stats(db_manager: DatabaseManager, table_name: str) -> p
         logger.info("Monthly charges data retrieved successfully.")
         return df
     
-    except sqlite3.Error as e:
+    except Exception as e:
         logger.error(f"Query execution failed: {e}")
         raise
 
@@ -59,7 +59,7 @@ def get_senior_churn_rate(db_manager: DatabaseManager, table_name: str) -> pd.Da
         logger.info("Retrieved senior citizen churn analysis.")
         return df
     
-    except sqlite3.Error as e:
+    except Exception as e:
         logger.error(f"Error fetching senior churn data: {e}")
         raise
 
@@ -80,6 +80,27 @@ def get_internet_service_churn(db_manager: DatabaseManager, table_name: str) -> 
         logger.info("Retrieved internet service churn data.")
         return df
     
-    except sqlite3.Error as e:
+    except Exception as e:
         logger.error(f"Error fetching internet service data: {e}")
+        raise
+
+def get_payment_method_stats(db_manager: DatabaseManager, table_name: str) -> pd.DataFrame:
+    query = f"""
+    select 
+        PaymentMethod,
+        count(*) as Total,
+        round(avg(MonthlyCharges), 2) as Avg_Monthly_Charge,
+        sum(case when Churn = 'Yes' then 1 else 0 end) * 100.0 / count(*) as Churn_Percentage
+    from {table_name}
+    group by PaymentMethod
+    order by Churn_Percentage desc;
+    """
+
+    try:
+        df = pd.read_sql_query(query, db_manager.conn)
+        logger.info("Retrieved payment method statistics.")
+        return df
+    
+    except Exception as e:
+        logger.error(f"Error fetching payment method data: {e}")
         raise
